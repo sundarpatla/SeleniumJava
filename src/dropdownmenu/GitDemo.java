570755d3de4c8752fb5a77bef7f06a10c06ab12b
sundarpatla <sundarpatla@gmail.com>
2025-12-30 06:59:38 +0530

    New class added and locators class modified

---
+++ src/dropdownmenu/GitDemo.java
@@ -0,0 +1,14 @@
+package dropdownmenu;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class GitDemo {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
--- src/locators/SeleniumLocators.java
+++ src/locators/SeleniumLocators.java
@@ -1,7 +1,5 @@
 package locators;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -9,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 class SeleniumLocators {
 	
@@ -18,16 +16,16 @@ class SeleniumLocators {
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		
-		driver = new EdgeDriver();
+		driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
-		;
+		
 	}
 
 	@Test
 	void idnameclassname() throws Exception {
 		
-		driver.get("https://www.techlearn.in/admin");
+		driver.get("https://www.techlearn.in/wp-admin");
 		
 		Thread.sleep(2000);
 		
