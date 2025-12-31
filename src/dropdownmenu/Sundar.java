072fbceee9979fa69a9c94873799956ba2fff963
sundarpatla <sundarpatla@gmail.com>
2025-12-31 06:46:50 +0530

    Sundar class TestNg reports.

--- src/dropdownmenu/Sundar.java
+++ src/dropdownmenu/Sundar.java
@@ -1,14 +1,22 @@
 package dropdownmenu;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Sundar {
+	
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
+	  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new EdgeDriver();
+	  
+	  driver.manage().window().maximize();
   }
 
 }
